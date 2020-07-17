b1313574baa228263c4ec0e8c43a66a0821adbc6
xinlin <xinlin.zhang@hotmail.com>
2020-07-17 16:38:22 +0800

    add sub_parsers of inline and infile

--- maily.py
+++ maily.py
@@ -14,8 +14,8 @@ import fcntl
 import mimetypes
 
 
-def maily(subject, text, attas, 
-          to, cc, bcc, from_addr, passwd, 
+def maily(subject, text, attas,
+          to, cc, bcc, from_addr, passwd,
           smtp, port, timeout, debuginfo):
     msg = MIMEMultipart('mixed')
     msg.attach(MIMEText(text, 'plain', 'utf-8'))
@@ -27,7 +27,7 @@ def maily(subject, text, attas,
     to.extend(bcc)
     for i in range(len(attas)):
         ftype, encoding = mimetypes.guess_type(attas[i])
-        if (ftype is None 
+        if (ftype is None
               or encoding is not None):
             ftype = 'application/octet-stream'
         maintype, subtype = ftype.split('/')
@@ -45,7 +45,7 @@ def maily(subject, text, attas,
             server = smtplib.SMTP_SSL(smtp, port, timeout)
         else:
             server = smtplib.SMTP(smtp, port, timeout)
-        if debuginfo: 
+        if debuginfo:
             if sys.version.split()[0][:3] >= '3.5':
                 server.set_debuglevel(2)
             else: server.set_debuglevel(1)
@@ -67,74 +67,81 @@ def check_addr(addr):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--subject', required=True,
+    subparser = parser.add_subparsers(dest='subcmd')
+    parser_inline = subparser.add_parser('inline', help='inline')
+    parser_infile = subparser.add_parser('infile', help='infile')
+    # subcommand: inline
+    parser_inline.add_argument('--subject', required=True,
             help='subject for this email')
-    parser.add_argument('--content', default=argparse.SUPPRESS,
+    parser_inline.add_argument('--content', default=argparse.SUPPRESS,
             help='email content')
-    parser.add_argument('--contype', default='plain', choices=['plain'],
+    parser_inline.add_argument('--contype',default='plain',choices=['plain'],
             help='specify the content type, default is plain text')
-    parser.add_argument('-a', '--attachment', nargs='+', default=[], 
+    parser_inline.add_argument('-a', '--attachment', nargs='+', default=[],
             help='attachments files')
-    parser.add_argument('--to', required=True, nargs='+',  
+    parser_inline.add_argument('--to', required=True, nargs='+',
             help='addresses of receivers')
-    parser.add_argument('--cc', nargs='+', default=[],  
+    parser_inline.add_argument('--cc', nargs='+', default=[],
             help='addresses of cc (carbon copy)')
-    parser.add_argument('--bcc', nargs='+', default=[], 
+    parser_inline.add_argument('--bcc', nargs='+', default=[],
             help='addresses of bcc (blind carbon copy)')
-    parser.add_argument('--fromaddr', required=True,
+    parser_inline.add_argument('--fromaddr', required=True,
             help='address of sender')
-    parser.add_argument('--passwd', required=True,
+    parser_inline.add_argument('--passwd', required=True,
             help='password of sender email account')
-    parser.add_argument('--smtp', required=True,
+    parser_inline.add_argument('--smtp', required=True,
             help='SMTP server of sender email account')
-    parser.add_argument('--port', type=int, default=587, choices=[25,465,587], 
+    parser_inline.add_argument('--port', type=int, default=587,
+            choices=[25,465,587],
             help='choose the port for SMTP server, default=587')
-    parser.add_argument('--timeout', type=int, default=3,  
+    parser_inline.add_argument('--timeout', type=int, default=3,
             help='connection timeout, default=3s')
-    parser.add_argument('--debuginfo', action='store_true',  
+    parser_inline.add_argument('--debuginfo', action='store_true',
             help='show debug info between SMTP server and maily')
     args = parser.parse_args()
-    # check subject
-    if args.subject.strip() == '':
-        print('Subject can not be empty.')
-        sys.exit(1)
-    # check content
-    if hasattr(args, 'content'):  # argument --content is present
-        # stdin must be empyt
-        if len(sys.stdin.readlines()) != 0:
-            print('Conflict content from both --content argument and stdin.')
+    if args.subcmd == 'inline':
+        # check subject
+        if args.subject.strip() == '':
+            print('Subject can not be empty.')
             sys.exit(1)
-    else:
-        setattr(args, 'content', ''.join(sys.stdin.readlines()))
-    # content tail
-    if args.contype == 'plain':
-        args.content += \
-          '\n\n\n\n--------\n'\
-          'This email was sent by maily (https://github.com/xinlin-z/maily),'\
-          ' which is a simple command line SMTP email sending tool in Python.'
-    # check attachment list
-    for item in args.attachment:
-        if os.path.isfile(item) is False:
-            print('Attachement %s is not a file.' % item)
-            sys.exit(1)
-    # check address list to, cc, bcc
-    for addr in args.to:
-        if check_addr(addr) is False:
-            print('%s: address format error in --to list.' % addr)
-            sys.exit(1)
-    for addr in args.cc:
-        if check_addr(addr) is False:
-            print('%s: address format error in --cc list.' % addr)
-            sys.exit(1)
-    for addr in args.bcc:
-        if check_addr(addr) is False:
-            print('%s: address format error in -bcc list.' % addr)
-            sys.exit(1)
-    # go
-    maily(args.subject, args.content, args.attachment,
-          args.to, args.cc, args.bcc, args.fromaddr, args.passwd, 
-          args.smtp, args.port, args.timeout, args.debuginfo)
-    
+        # check content
+        if hasattr(args, 'content'):  # argument --content is present
+            # stdin must be empyt
+            if len(sys.stdin.readlines()) != 0:
+                print('content conflict from both cmd argument and stdin.')
+                sys.exit(1)
+        else:
+            setattr(args, 'content', ''.join(sys.stdin.readlines()))
+        # content tail
+        if args.contype == 'plain':
+            args.content += \
+              '\n\n\n\n--------\n'\
+              'sent by maily --> https://github.com/xinlin-z/maily'
+        # check attachment list
+        for item in args.attachment:
+            if os.path.isfile(item) is False:
+                print('Attachement %s is not a file.' % item)
+                sys.exit(1)
+        # check address list to, cc, bcc
+        for addr in args.to:
+            if check_addr(addr) is False:
+                print('%s: address format error in --to list.' % addr)
+                sys.exit(1)
+        for addr in args.cc:
+            if check_addr(addr) is False:
+                print('%s: address format error in --cc list.' % addr)
+                sys.exit(1)
+        for addr in args.bcc:
+            if check_addr(addr) is False:
+                print('%s: address format error in -bcc list.' % addr)
+                sys.exit(1)
+        # go
+        maily(args.subject, args.content, args.attachment,
+              args.to, args.cc, args.bcc, args.fromaddr, args.passwd,
+              args.smtp, args.port, args.timeout, args.debuginfo)
+    else:  # infile
+        print('under development')
+
 
 if __name__ == '__main__':
     fcntl.fcntl(sys.stdin, fcntl.F_SETFL, os.O_NONBLOCK)
