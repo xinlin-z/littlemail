a1ad625a2649710ec82be5fc42267a6ff5e8af87
xinlin <xinlin.zhang@hotmail.com>
2023-10-05 09:41:33 +0800

    tweaks

--- api_test.py
+++ api_test.py
@@ -1,15 +1,15 @@
 from littlemail import send_email
 
 
-# Just fill the 4 line below and run,
-# you can test send_email API provided by littlemail!:)
+# For simplicity, just fill the 4 line below and run,
+# you can test send_email API provided by littlemail.
 fromaddr = ''
 to = ['']
 smtp = ''
 passwd = ''
 
 
-send_email('send test', text='test email sent by littlemail!',
+send_email('send test', text='Test email sent by littlemail!',
                         to=to,
                         fromaddr=fromaddr,
                         smtp=smtp,
